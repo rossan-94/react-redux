74e7b77464a6ad0245aa34b0cee93b5375916889
Rossan Pal <raushanpal1194@gmail.com>
2019-12-25 16:39:20 +0530

    added git ignore

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<> Hello Refresh 12345</>, document.getElementById('app'))
+ReactDOM.render(<> Welcome to react world </>, document.getElementById('app'))
 module.hot.accept()
